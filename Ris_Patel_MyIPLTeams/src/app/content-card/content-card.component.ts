538447c6c128c10cad5023ebb9855946154a1580
Rishi Patel <rp158@myscc.ca>
2023-02-10 17:13:32 -0500

    updated package.json and created content-list from generateComponenet

--- Ris_Patel_MyIPLTeams/src/app/content-card/content-card.component.ts
+++ Ris_Patel_MyIPLTeams/src/app/content-card/content-card.component.ts
@@ -1,42 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
-import { ContentList } from '../helper-files/content-list';
 
 @Component({
   selector: 'app-content-card',
   templateUrl: './content-card.component.html',
   styleUrls: ['./content-card.component.scss']
 })
-export class ContentCardComponent {
-
-  iplteams : ContentList = new ContentList();
-
-  iplteam1 : Content = {
-    id: 0,
-    title: 'Chennai Super Kings',
-    description: 'Chennai Super Kings is an Indian Premier League franchise cricket team based in Chennai, Tamil Nadu, India. Founded in 2008, the team plays its home matches at the M. A. Chidambaram Stadium in Chennai. The team is owned by India Cements Limited through the Chennai Super Kings Cricket Limited holding company. ',
-    creator: 'Chennai Super Kings Cricket Ltd'
-  }
-  iplteam2 : Content = {
-    id: 1,
-    title: 'Mumbai Indians',
-    description: 'Mumbai Indians are a franchise cricket team based in Mumbai, Maharashtra, that competes in the Indian Premier League and is the most successful team of the tournament with 5 titles. Founded in 2008, the team is owned by Indias biggest conglomerate, Reliance Industries, through its 100% subsidiary Indiawin Sports',
-    creator: 'Ambani Group'
-  }
-  iplteam3 : Content = {
-    id: 2,
-    title: 'Royal Challengers Banglore',
-    description: 'Royal Challengers Bangalore are a franchise cricket team based in Bengaluru, Karnataka, that plays in the Indian Premier League. It was founded in 2008 by United Spirits and named after the companys liquor brand Royal Challenge.',
-    creator: 'Vijay Maliya'
+export class ContentCardComponent implements OnInit {
+ 
+  @Input() content?: Content;
+  private _item: Content[];
+  constructor() {
+    this._item=[];
   }
 
-  constructor(){
-    this.iplteams.addFunction(this.iplteam1);
-    this.iplteams.addFunction(this.iplteam2);
-    this.iplteams.addFunction(this.iplteam3);
+  getItem(): Content[] {
+    return this._item;
   }
 
-  displayMyIndex(index : number): string {
-    return this.iplteams.printIndex(index);
+  pritnItem() {
+    console.log(this.content?.id);
+    console.log(this.content?.title);
+    console.log(this.content?.description);
+    console.log(this.content?.creator);
   }
+
+  PhotoView(id: number, title: string) {
+    console.log(`Content id: ${id}, title: ${title}`);
+  }
+
+  PhotoView2() {
+    console.log(`Content ID: ${this.content?.id} Title: ${this.content?.title}`);
+  }
+  
+  ngOnInit(): void {
+  }
+  
 }
--- Ris_Patel_MyIPLTeams/src/app/helper-files/content-interface.ts
+++ Ris_Patel_MyIPLTeams/src/app/helper-files/content-interface.ts
@@ -1,9 +1,11 @@
+import { Optional } from "@angular/core";
+
 export interface Content {
     id: number;
     title: string;
     description: string;
+    creator: string;
     imgURL?: string;
     type?: string;
     tags?: string [];
-    creator: string;
 }
