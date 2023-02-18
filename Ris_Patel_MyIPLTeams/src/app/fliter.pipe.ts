1b17d2ee6583d843d1c7d4afb3e6bfbe1cecccbe
Rishi Patel <rp158@myscc.ca>
2023-02-17 20:20:53 -0500

    Error solving for filter.pipe.tts

--- Ris_Patel_MyIPLTeams/src/app/fliter.pipe.ts
+++ Ris_Patel_MyIPLTeams/src/app/fliter.pipe.ts
@@ -1,12 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Content } from './helper-files/content-interface';
 
 @Pipe({
   name: 'fliter'
 })
 export class FliterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
+  transform(content: Content[]): any {
+    if(!content){
+      return content;
+    }
+    return content.filter((content) => {
+    //return item.type?.length;
+    return content.type;//?.length || content.type == 'Anime' || content.type == 'Manga';
+  });
+ }
 
 }
