0920b5a09de6759bb87b79ee9eb1cac53151cecc
Rishi Patel <rp158@myscc.ca>
2023-04-06 00:31:50 -0400

    content-list-interface.html

--- Ris_Patel_MyIPLTeams/src/app/helper-files/content-interface.ts
+++ Ris_Patel_MyIPLTeams/src/app/helper-files/content-interface.ts
@@ -1,10 +1,10 @@
 import { Optional } from "@angular/core";
 
 export interface Content {
-    id: number;
+    id?: number;
     title: string;
     description: string;
-    creator: string;
+    creator?: string;
     imgURL?: string;
     type?: string;
     tags?: string [];
