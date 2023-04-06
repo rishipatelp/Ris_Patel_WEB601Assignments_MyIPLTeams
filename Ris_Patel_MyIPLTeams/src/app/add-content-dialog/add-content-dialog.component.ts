b36efa52a471cb5ce1c364ea1fb1070cf436d770
Rishi Patel <rp158@myscc.ca>
2023-04-06 17:49:56 -0400

    Adding constructure to .ts file

--- Ris_Patel_MyIPLTeams/src/app/add-content-dialog/add-content-dialog.component.ts
+++ Ris_Patel_MyIPLTeams/src/app/add-content-dialog/add-content-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ModifyContentComponent } from '../modify-content/modify-content.component';
 
 @Component({
   selector: 'app-add-content-dialog',
@@ -7,5 +8,18 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./add-content-dialog.component.scss']
 })
 export class AddContentDialogComponent {
-  dialogTitle = "Add IPLTeamsTypes";
+  dialogTitle = "Add IPLTeamsTypes"; 
+  
+  constructor(
+    public dialogRef: MatDialogRef<ModifyContentComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ){}
+
+  onCancelClick(): void {
+    this.dialogRef.close();
+  }
+  
+  onAddClick(): void {
+    this.dialogRef.close(this.data);
+  }
 }
