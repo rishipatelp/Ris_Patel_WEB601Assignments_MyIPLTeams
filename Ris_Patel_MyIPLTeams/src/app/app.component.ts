f226f499b6da47ca0bad79c1e2c9a95936ab7926
Rishi Patel <rp158@myscc.ca>
2023-04-19 19:45:52 -0400

    package.json

--- Ris_Patel_MyIPLTeams/src/app/app.component.ts
+++ Ris_Patel_MyIPLTeams/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UpdateService } from './update.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ris_Patel_MyIPLTeams';
+}
+export class AppComponent {
+  constructor(private updateService: UpdateService) {}
 }
---
+++ Ris_Patel_MyIPLTeams/src/app/update.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { interval } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UpdateService {
+
+  constructor(private swUpdate: SwUpdate, private snackBar: MatSnackBar) {
+    if (swUpdate.isEnabled) {
+      swUpdate.available.subscribe(() => {
+        const snack = this.snackBar.open('Update Available', 'Reload', {
+          duration: 6000,
+        });
+
+        snack.onAction().subscribe(() => {
+          swUpdate.activateUpdate().then(() => document.location.reload());
+        });
+      });
+      // Implement long polling
+      interval(30 * 60 * 1000).pipe(
+        mergeMap(() => swUpdate.checkForUpdate())
+      ).subscribe();
+    }
+  }
+}
